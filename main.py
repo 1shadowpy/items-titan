f1cdb203b443ad669e385e1d2e013d3104290514
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-05-22 11:11:52 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# pythonizohi = {}
+# pythonizohi['float']= 'noaniq'
+# pythonizohi['if']= 'agar'
+# pythonizohi['else']= 'yoki'
+# pythonizohi['key']= 'kalit'
+# pythonizohi['main']='asosiy'
+#
+# print(pythonizohi.keys())
+# print(pythonizohi.values())
+# for g in pythonizohi.items():
+#    print(sorted(g))
+#
+# davlatlar = {}
+#
+# davlatlar['uzbekiston']='toshkent'
+# davlatlar['rasiya']= 'moskva'
+# davlatlar['ukraina']= 'kiyev'
+# davlatlar['braziliya'] = 'braziliya'
+# davlatlar['tojikiston'] = 'dushnambe'
+# for i in davlatlar.items():
+#     print(sorted(i))
+
+#
+# dp= {"AQSH":"Washington D.C.","Italiya":"Rim","Malayziya":"Kuala-Lumpur","O\'zbekiston":"Toshkent","Qirg\'iziston":"Nursulton","Qozog\'iston":"Bishkek","Rossiya":"Moskva","Singapur":"Sungapur","Tojikiston":"Dushanbe"}
+# f= input("Qaysi davlatning poytaxtini bilishni istaysiz?")
+# if f in dp:
+#     print(f'{f}ning poytaxti {dp[f]}')
+# else:
+#     print(" Kechirasiz bu davlat xaqida ma\'lumot yo\'q")
+#
+# taomlar= {"non":"2 000 so\'m","osh":"20 000 so\'m","somsa":"5 000 so\'m","qiyma":"9 000 so\'m","baliq":"30 000 so\'m","pepsi":"9 000 so\'m","lavash":"17 000 so\'m","xod-dog":"20 000 so\'m","gamburger":"16 000 so\'m","asu":"4 000 so\'m"}
+# for a in range(1,4):
+#     i= input(f'{a}-taomni kiriting:')
+#     if i in taomlar:
+#         print(f"{i}= {taomlar[i]}")
+#     else:
+#         print(f"{i} bizda yo\'q")
